046c1b010febe83b61a7231f3a0cb88d8b519806
davidgsanti9-ai <davidgsanti9-ai>
2026-01-11 19:09:11 -0600

    firt-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { LightSwitch } from './components/LightSwitch';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <LightSwitch />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/LightSwitch.jsx
@@ -0,0 +1,27 @@
+
+
+export function LightSwitch() {
+
+
+    const handleClick = () => {
+        
+        let bodyStyle = document.body.style;
+
+        if ( bodyStyle.backgroundColor === 'black' ) {
+            bodyStyle.backgroundColor = 'white'
+        } else {
+            bodyStyle.backgroundColor = 'black'
+        }
+
+    }
+
+
+    return (
+        <button onClick={ handleClick } >
+            Alterna las luces
+        </button>
+    );
+
+}
+
+
